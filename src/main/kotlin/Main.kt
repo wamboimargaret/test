b6da691fc4e84f69a908e0ec15496d3213b3c2c3
student <margarettwmboi@gmail.com>
2023-03-09 08:53:46 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+fun main() {
+var x =words("Pretoria","Nairobi")
+    println(x)
+    num()
+    wordss("fify five")
+    v()
+
+}
+fun words(name:String,name3:String):Char{
+ var character = name[0]
+    return character
+
+}
+fun num(){
+    var nums= arrayOf(3,4,5,6)
+    var small = nums.min()
+}
+fun wordss(word:String){
+    var b = word.split("fifty five", limit = 0)
+    println(b)
+}
+fun v(){
+    var g= arrayOf("null","school",)
+    var t =
+    println(g.contentToString())
+}
+
